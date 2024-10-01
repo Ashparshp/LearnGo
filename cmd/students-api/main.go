67c4a756d6fa2270c57e52208195c503cf827a9b
Ashparsh Pandey <ashparsh.connects@gmail.com>
2024-10-01 18:08:30 +0530

    Server Setup
    Signed-off-by: Ashparsh Pandey <ashparsh.connects@gmail.com>

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/ashparshp/learngo/internal/config"
+)
+
 
 func main() {
-	fmt.Println("Welcome to api")
+	cfg := config.MustLoad()
+
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func (w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	server := http.Server{
+		Addr: cfg.Addr,
+		Handler: router,
+	}
+
+	slog.Info("Starting server", slog.String("address", cfg.Addr))
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+	} ()
+	<-done
+	
+	slog.Info("Sutting down server")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := server.Shutdown(ctx)
+
+	if err != nil {
+		slog.Error("Server shutdown failed", slog.String("error", err.Error()))
+	} else {
+		slog.Info("Server stopped")
+	}
 }
